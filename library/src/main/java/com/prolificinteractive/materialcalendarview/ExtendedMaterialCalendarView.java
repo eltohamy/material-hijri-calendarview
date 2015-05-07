435e754451ef673f1b3d3a55bd8e733327ba8d5f
ChahineM <chahine.m@hotmail.com>
2015-05-07 00:02:27 -0400

    extended view animations

--- library/src/main/java/com/prolificinteractive/materialcalendarview/ExtendedMaterialCalendarView.java
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/ExtendedMaterialCalendarView.java
@@ -1,9 +1,11 @@
 package com.prolificinteractive.materialcalendarview;
 
+import android.animation.Animator;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -12,16 +14,19 @@ import java.util.Date;
 
 public class ExtendedMaterialCalendarView extends FrameLayout implements OnDateChangedListener {
 
-    final static SimpleDateFormat fullDayFormat = new SimpleDateFormat("EEEE");
-    final static SimpleDateFormat monthFormat = new SimpleDateFormat("MMM");
-    final static SimpleDateFormat dayFormat = new SimpleDateFormat("d");
-    final static SimpleDateFormat yearFormat = new SimpleDateFormat("y");
+    private final static SimpleDateFormat FULL_DAY_FORMAT = new SimpleDateFormat("EEEE");
+    private final static SimpleDateFormat MONTH_FORMAT = new SimpleDateFormat("MMM");
+    private final static SimpleDateFormat DAY_FORMAT = new SimpleDateFormat("d");
+    private final static SimpleDateFormat YEAR_FORMAT = new SimpleDateFormat("y");
 
-    final MaterialCalendarView mcv;
-    final TextView fullDay;
-    final TextView month;
-    final TextView day;
-    final TextView year;
+    private final MaterialCalendarView mcv;
+    private final TextView fullDay;
+    private final TextView month;
+    private final TextView day;
+    private final TextView year;
+
+    private OnDateChangedListener onDateChangedListener;
+    private CalendarDay previousDay;
 
     public ExtendedMaterialCalendarView(Context context) {
         this(context, null);
@@ -47,15 +52,86 @@ public class ExtendedMaterialCalendarView extends FrameLayout implements OnDateC
         return mcv;
     }
 
-    public void setSelectedDate(final Date date) {
-        fullDay.setText(fullDayFormat.format(date));
-        month.setText(monthFormat.format(date));
-        day.setText(dayFormat.format(date));
-        year.setText(yearFormat.format(date));
+    public void setSelectedDate(final CalendarDay calendarDay) {
+        final Date date = calendarDay.getDate();
+        if (previousDay == null) {
+            fullDay.setText(FULL_DAY_FORMAT.format(date));
+            month.setText(MONTH_FORMAT.format(date));
+            day.setText(DAY_FORMAT.format(date));
+            year.setText(YEAR_FORMAT.format(date));
+        } else {
+            // full day
+            final String futureFullDay = FULL_DAY_FORMAT.format(date);
+            if (!fullDay.getText().toString().equalsIgnoreCase(futureFullDay)) {
+                animateView(fullDay, futureFullDay, calendarDay);
+            }
+            // month
+            final String futureMonth = MONTH_FORMAT.format(date);
+            if (!month.getText().toString().equalsIgnoreCase(futureMonth)) {
+                animateView(month, futureMonth, calendarDay);
+            }
+            // day
+            final String futureDay = DAY_FORMAT.format(date);
+            if (!day.getText().toString().equalsIgnoreCase(futureDay)) {
+                animateView(day, futureDay, calendarDay);
+            }
+            // year
+            final String futureYear = YEAR_FORMAT.format(date);
+            if (!year.getText().toString().equalsIgnoreCase(futureYear)) {
+                animateView(year, futureYear, calendarDay);
+            }
+        }
+    }
+
+    private void animateView(final TextView view, final String text, final CalendarDay day) {
+
+        final int yTranslation =
+                getResources().getDimensionPixelSize(R.dimen.mcv_default_title_y_translation)
+                        * (previousDay.isBefore(day) ? 1 : -1);
+        final DecelerateInterpolator interpolator = new DecelerateInterpolator(2f);
+        final int duration = getResources().getInteger(android.R.integer.config_shortAnimTime);
+        final int startDelay = getResources().getInteger(R.integer.mcv_default_title_start_delay);
+
+        view.animate().cancel();
+        view.setTranslationY(0);
+        view.animate()
+                .translationY(yTranslation * -1)
+                .alpha(0)
+                .setDuration(duration)
+                .setStartDelay(startDelay)
+                .setInterpolator(interpolator)
+                .setListener(new AnimatorListener() {
+
+                    @Override
+                    public void onAnimationCancel(Animator animator) {
+                        view.setTranslationY(0);
+                        view.setAlpha(1);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animator) {
+                        view.setText(text);
+                        view.setTranslationY(yTranslation);
+                        view.animate()
+                                .translationY(0)
+                                .alpha(1)
+                                .setDuration(duration)
+                                .setInterpolator(interpolator)
+                                .setListener(new AnimatorListener())
+                                .start();
+                    }
+                }).start();
+    }
+
+    public void setOnDateChangedListener(final OnDateChangedListener listener) {
+        this.onDateChangedListener = listener;
     }
 
     @Override
     public void onDateChanged(MaterialCalendarView widget, CalendarDay date) {
-        setSelectedDate(date.getDate());
+        setSelectedDate(date);
+        if (onDateChangedListener != null)
+            onDateChangedListener.onDateChanged(widget, date);
+        previousDay = date;
     }
 }
--- sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/ExtendedActivity.java
+++ sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/ExtendedActivity.java
@@ -1,15 +1,25 @@
 package com.prolificinteractive.materialcalendarview.sample;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.ExtendedMaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateChangedListener;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 
-public class ExtendedActivity extends ActionBarActivity {
+public class ExtendedActivity extends AppCompatActivity implements OnDateChangedListener {
+
+    private static final DateFormat FORMATTER = SimpleDateFormat.getDateInstance();
+
     @InjectView(R.id.extended_mcv)
     ExtendedMaterialCalendarView extendedMcv;
 
@@ -18,5 +28,11 @@ public class ExtendedActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_extended);
         ButterKnife.inject(this);
+        extendedMcv.setOnDateChangedListener(this);
+    }
+
+    @Override
+    public void onDateChanged(MaterialCalendarView widget, CalendarDay date) {
+        Toast.makeText(this, FORMATTER.format(date.getDate()), Toast.LENGTH_SHORT).show();
     }
 }
